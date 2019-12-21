ba4667741f3661f71bdcfe4260f957f2d440d716
Éloi Draï <eloidrai@gmail.com>
2019-12-21 23:34:19 +0100

    Calcul des trajectoires

--- script.js
+++ script.js
@@ -2,26 +2,31 @@ const VIDE = 0, DEVIE = 1, REFLECHI = 2, ABSORBE = 3, ATOME = 4;
 
 class BlackBox {
     constructor(L, l, nbAtomes){
-        this.L = L, this.l = l, this.nbAtomes = nbAtomes;
-        this.creerGrille(this.L, this.l, this.nbAtomes);
+        this.L = L, this.l = l, this.numAtomes = nbAtomes;
+        this.creerGrille(this.L, this.l, this.numAtomes);
         console.log("La partie peut commencer !");
     }
     
     creerGrille(L, l, nbAtomes){
         /* Création du plateau */
         this.grille = Array(l+2).fill().map(e=>Array(L+2).fill().map(e=>{return {statut: VIDE};})); // Génère une grille avec un contour
+        this.tour = {};     // Tableau associant le numéro d'une case périphérique avec ses coordonées
         for (let c=1; c<=(L+l)*2; c++){
             if (c<=l){                          // Bord gauche
-                this.grille[c][0].nb = c;
+                this.grille[c][0].num = c;
+                this.tour[c] = new Rayon(0, c, 1, 0);
             } else if (c<=l+L){                 // Bas
-                this.grille[l+1][c-l].nb = c;
+                this.grille[l+1][c-l].num = c;
+                this.tour[c] = new Rayon(c-l, l+1, 0, -1);
             } else if (c<=l+L+l){               // Bord droit
-                this.grille[l-(c-l-L)+1][L+1].nb = c;
+                this.grille[l-(c-l-L)+1][L+1].num = c;
+                this.tour[c] = new Rayon(L+1, l-(c-l-L)+1, -1, 0);
             } else {                            // Haut
-                this.grille[0][L-(c-l-L-l)+1].nb = c;
+                this.grille[0][L-(c-l-L-l)+1].num = c;
+                this.tour[c] = new Rayon(L-(c-l-L-l)+1, 0, 0, 1);
             }
         }
-        this.grille[0][0].nb = NaN, this.grille[l+1][0].nb = NaN, this.grille[l+1][L+1].nb = NaN, this.grille[0][L+1].nb = NaN; // On ne peut pas aller derrière les coins
+        this.grille[0][0].num = 0, this.grille[l+1][0].num = 0, this.grille[l+1][L+1].num = 0, this.grille[0][L+1].num = 0; // On ne peut pas aller derrière les coins
         
         /* Placement des atomes */
         for (let i=0; i<nbAtomes; i++){
@@ -34,15 +39,50 @@ class BlackBox {
                     this.grille[y][x].statut = ABSORBE;              // Les cases sur lesquelles on est absorbé
                 } else {
                      // Réfléchi si deux champs se chevauchent ou si on n'est pas sur le plateau
-                    if (this.grille[y][x].statut === DEVIE || typeof this.grille[y][x].nb === "number") this.grille[y][x].statut = REFLECHI;
+                    if (this.grille[y][x].statut === DEVIE || typeof this.grille[y][x].num === "number") this.grille[y][x].statut = REFLECHI;
                     if (this.grille[y][x].statut === VIDE) this.grille[y][x].statut = DEVIE;
-                    
                 }
             }
         }
     }
+    
+    coup(num){
+        let {x, y, deplacementX, deplacementY} = this.tour[num];
+        while (true) {
+            console.log(x, y);
+            switch (this.grille[y][x].statut){          // Instructions selon le statut de la case
+                case VIDE:
+                    break;
+                case DEVIE:
+                    if (deplacementY === 0){                                        // Déplacement d'incidence horizontal
+                        deplacementY = (this.grille[y-1][x].statut === ABSORBE)? 1: -1;
+                        deplacementX = 0;
+                        break;
+                    } else {
+                        deplacementX = (this.grille[y][x-1].statut === ABSORBE)? 1: -1;
+                        deplacementY = 0;
+                        break;
+                    }
+                case REFLECHI:
+                    return num;
+                    break;
+                case ABSORBE:
+                    return 0;
+                    break;
+            }
+            x+= deplacementX;
+            y+= deplacementY;
+            if (this.grille[y][x].num) return this.grille[y][x].num;
+        }
+    }
 }
 
+class Rayon {        
+    constructor(x, y, deplacementX, deplacementY){
+        this.x = x, this.y = y;
+        this.deplacementX = deplacementX, this.deplacementY = deplacementY;
+    }
+}
 
 // Débogage
 
@@ -51,7 +91,7 @@ function afficherGrille(g){
     for (let y in g) {
         html+="<tr>";
         for (let x in g[y]){
-            html+=`<td class="${(typeof g[y][x].nb === "number")? "gris": ""}">`+g[y][x].statut+"</td>";
+            html+=`<td class="${(typeof g[y][x].num === "number")? "gris": ""}">`+g[y][x].statut+"</td>";
         }
         html+="</tr>";
     }
@@ -59,4 +99,5 @@ function afficherGrille(g){
     document.body.innerHTML = html;
 }
 
-afficherGrille((new BlackBox(4,4, 2)).grille)
+const bb = new BlackBox(4,4, 2);
+afficherGrille(bb.grille);
